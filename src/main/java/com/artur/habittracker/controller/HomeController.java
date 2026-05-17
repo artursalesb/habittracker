9b9b028b3eaf54b2afcf891dbb3acdd0327660aa
ArturCesarGit <168553261+ArturCesarGit@users.noreply.github.com>
2026-05-17 02:45:01 -0300

    primeiro REST endpoint

---
+++ src/main/java/com/artur/habittracker/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.artur.habittracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Habit Tracker API is running!";
+    }
+}
